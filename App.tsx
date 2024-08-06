6ac43aa6e69073eb1d8224cee339666a983a8f2d
selalmaz <ahmetsllmz@gmail.com>
2024-08-06 12:12:16 +0300

    comment line

--- App.tsx
+++ App.tsx
@@ -3,6 +3,14 @@ import { Provider } from 'react-redux';
 import { store } from './src/store';
 import Counter from './src/Counter';
 
+
+//store: Global olarak saklamak istediğimiz verileri oluşturduğumuz depo alanı
+
+//reducer: Global verileri güncellediğimiz fonksiyonlar bütünü
+
+//provider: Component’lerimizi sarmallayan (bir nevi yöneten) context bileşeni
+
+
 const App = () => {
   return (
     <Provider store={store}>
